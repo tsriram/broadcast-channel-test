41e43bad90b38f7876562475b529181003d47ef7
Sriram Thiagarajan <sri.sjc@gmail.com>
2020-03-02 17:01:31 +0530

    test broadcast channel

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const [message, setMessage] = React.useState("test");
+
+  const channel = React.useMemo(() => new BroadcastChannel("message"), []);
+  channel.onmessage = message => {
+    console.log("message: ", message);
+    setMessage(message.data);
+  };
+
+  React.useEffect(() => {
+    return channel.close;
+  }, [channel.close]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setMessage(value);
+    channel.postMessage(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 50 }}>
+      <h1>Broadcast Channel Test</h1>
+      <input
+        autoFocus
+        onChange={handleChange}
+        placeholder="Type a message here..."
+        style={{ width: 300, height: 50 }}
+        value={message}
+      />
+      <strong>Message:</strong>
+      <p>{message}</p>
     </div>
   );
 }
